cf2e7c3acd6a3ff45cb8aa5770839ec911b32da0
Hasnat <hasnat@get-licensed.co.uk>
2025-12-11 18:00:33 +0500

    unsafe registrar

--- ios/Classes/FANPlugin.swift
+++ ios/Classes/FANPlugin.swift
@@ -74,25 +74,31 @@ public class FANPlugin: NSObject, FlutterPlugin {
             withId: FANConstant.NATIVE_BANNER_AD_CHANNEL
         )
 
-        // Main plugin channel
-        let pluginChannel = FlutterMethodChannel(
-            name: FANConstant.MAIN_CHANNEL,
-            binaryMessenger: registrar.messenger()
-        )
-        _ = FANPluginFactory(_channel: pluginChannel)
+        // Capture registrar safely for async context
+        nonisolated(unsafe) let unsafeRegistrar = registrar
+        
+        // Initialize plugins on MainActor
+        Task { @MainActor in
+            // Main plugin channel
+            let pluginChannel = FlutterMethodChannel(
+                name: FANConstant.MAIN_CHANNEL,
+                binaryMessenger: unsafeRegistrar.messenger()
+            )
+            _ = FANPluginFactory(_channel: pluginChannel)
 
-        // Interstitial
-        let interstitialChannel = FlutterMethodChannel(
-            name: FANConstant.INTERSTITIAL_AD_CHANNEL,
-            binaryMessenger: registrar.messenger()
-        )
-        _ = FacebookAudienceNetworkInterstitialAdPlugin(_channel: interstitialChannel)
+            // Interstitial
+            let interstitialChannel = FlutterMethodChannel(
+                name: FANConstant.INTERSTITIAL_AD_CHANNEL,
+                binaryMessenger: unsafeRegistrar.messenger()
+            )
+            _ = FacebookAudienceNetworkInterstitialAdPlugin(_channel: interstitialChannel)
 
-        // Rewarded
-        let rewardedChannel = FlutterMethodChannel(
-            name: FANConstant.REWARDED_VIDEO_CHANNEL,
-            binaryMessenger: registrar.messenger()
-        )
-        _ = FacebookAudienceNetworkRewardedAdPlugin(_channel: rewardedChannel)
+            // Rewarded
+            let rewardedChannel = FlutterMethodChannel(
+                name: FANConstant.REWARDED_VIDEO_CHANNEL,
+                binaryMessenger: unsafeRegistrar.messenger()
+            )
+            _ = FacebookAudienceNetworkRewardedAdPlugin(_channel: rewardedChannel)
+        }
     }
 }
