2da37d9d950020219bafcf947fd94fb8ddb6c45e
Hasnat <hasnat@get-licensed.co.uk>
2025-12-11 17:20:43 +0500

    Update FANPlugin.swift

--- ios/Classes/FANPlugin.swift
+++ ios/Classes/FANPlugin.swift
@@ -1,3 +1,50 @@
+// import Flutter
+// import UIKit
+
+// public class FANPlugin: NSObject, FlutterPlugin {
+
+//     public static func register(with registrar: FlutterPluginRegistrar) {
+//         print(">> FAN Plugin register")
+
+//         // Factories CAN be registered from main actor safely
+//         registrar.register(
+//             FacebookAudienceNetworkBannerAdFactory(_registrar: registrar),
+//             withId: FANConstant.BANNER_AD_CHANNEL
+//         )
+
+//         registrar.register(
+//             FacebookAudienceNetworkNativeAdFactory(_registrar: registrar),
+//             withId: FANConstant.NATIVE_AD_CHANNEL
+//         )
+
+//         registrar.register(
+//             FacebookAudienceNetworkNativeBannerAdFactory(_registrar: registrar),
+//             withId: FANConstant.NATIVE_BANNER_AD_CHANNEL
+//         )
+
+//         // Main plugin channel
+//         let pluginChannel = FlutterMethodChannel(
+//             name: FANConstant.MAIN_CHANNEL,
+//             binaryMessenger: registrar.messenger()
+//         )
+//         FANPluginFactory(_channel: pluginChannel)
+
+//         // Interstitial
+//         let interstitialChannel = FlutterMethodChannel(
+//             name: FANConstant.INTERSTITIAL_AD_CHANNEL,
+//             binaryMessenger: registrar.messenger()
+//         )
+//         _ = FacebookAudienceNetworkInterstitialAdPlugin(_channel: interstitialChannel)
+
+//         // Rewarded
+//         let rewardedChannel = FlutterMethodChannel(
+//             name: FANConstant.REWARDED_VIDEO_CHANNEL,
+//             binaryMessenger: registrar.messenger()
+//         )
+//         _ = FacebookAudienceNetworkRewardedAdPlugin(_channel: rewardedChannel)
+//     }
+// }
+
 import Flutter
 import UIKit
 
@@ -6,7 +53,7 @@ public class FANPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         print(">> FAN Plugin register")
 
-        // Factories CAN be registered from main actor safely
+        // Register factories (safe, not actor isolated)
         registrar.register(
             FacebookAudienceNetworkBannerAdFactory(_registrar: registrar),
             withId: FANConstant.BANNER_AD_CHANNEL
@@ -22,25 +69,30 @@ public class FANPlugin: NSObject, FlutterPlugin {
             withId: FANConstant.NATIVE_BANNER_AD_CHANNEL
         )
 
-        // Main plugin channel
-        let pluginChannel = FlutterMethodChannel(
-            name: FANConstant.MAIN_CHANNEL,
-            binaryMessenger: registrar.messenger()
-        )
-        FANPluginFactory(_channel: pluginChannel)
+        // Hop to MainActor for plugin initialization
+        Task { @MainActor in
+            print(">> FAN Plugin initializing components on MainActor")
 
-        // Interstitial
-        let interstitialChannel = FlutterMethodChannel(
-            name: FANConstant.INTERSTITIAL_AD_CHANNEL,
-            binaryMessenger: registrar.messenger()
-        )
-        _ = FacebookAudienceNetworkInterstitialAdPlugin(_channel: interstitialChannel)
+            // Main plugin channel
+            let pluginChannel = FlutterMethodChannel(
+                name: FANConstant.MAIN_CHANNEL,
+                binaryMessenger: registrar.messenger()
+            )
+            _ = FANPluginFactory(_channel: pluginChannel)
 
-        // Rewarded
-        let rewardedChannel = FlutterMethodChannel(
-            name: FANConstant.REWARDED_VIDEO_CHANNEL,
-            binaryMessenger: registrar.messenger()
-        )
-        _ = FacebookAudienceNetworkRewardedAdPlugin(_channel: rewardedChannel)
+            // Interstitial
+            let interstitialChannel = FlutterMethodChannel(
+                name: FANConstant.INTERSTITIAL_AD_CHANNEL,
+                binaryMessenger: registrar.messenger()
+            )
+            _ = FacebookAudienceNetworkInterstitialAdPlugin(_channel: interstitialChannel)
+
+            // Rewarded
+            let rewardedChannel = FlutterMethodChannel(
+                name: FANConstant.REWARDED_VIDEO_CHANNEL,
+                binaryMessenger: registrar.messenger()
+            )
+            _ = FacebookAudienceNetworkRewardedAdPlugin(_channel: rewardedChannel)
+        }
     }
 }
